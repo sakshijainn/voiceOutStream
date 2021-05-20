6421ca29823fcecd82755014680f4faea00d9618
sakshijainn <sakshijjain1997@gmail.com>
2021-05-20 14:25:22 +0530

    playlist management

--- video-library/src/Reducer/videoReducer.js
+++ video-library/src/Reducer/videoReducer.js
@@ -1,116 +1,105 @@
+export default function videoReducer(state, { type, payload }) {
+  switch (type) {
+    case "DATA_FROM_SERVER":
+      return {
+        ...state,
+        videos: payload,
+      };
 
-  
-  export default function videoReducer(state, {
-    type,
-    payload
-  }) {
-    switch (type) {
-      case "DATA_FROM_SERVER":
-        return {
-          ...state,
-          videos: payload
-        };
-
-
-      case "LIKE":
-        {
-        console.log(payload);
-        return {
-          ...state,
-          likedVideos: state.likedVideos.includes(payload) ?
-            state.likedVideos.filter((id) => id !== payload) :
-            state.likedVideos.concat(payload)
-        }
-      }
-
-      case "WATCH_LATER":
-        return {
-          ...state,
-          watchLater: state.watchLater.includes(payload) ?
-            state.watchLater.filter((id) => id !== payload) :
-            state.watchLater.concat(payload)
-        };
-
-      case "HISTORY":
-        return {
-          ...state,
-          historyVideos: state. historyVideos.some((videoId) => videoId === payload)
-            ? state. historyVideos
-                .filter((videoId) => videoId !== payload)
-                .concat(payload)
-            : state. historyVideos.concat(payload),
-        };
-
-
-
-        case "CREATE_NEW_PLAYLIST":
-          return { ...state, playlist: [...state.playlist, payload] 
-         
-          }
-         
-          /*playlist: [{ id: uuidv4(), name: 'sakshijain', videos: [{id,title,imageSrc,..}] }],*/
-
-
-          // case "ADD_TO_PLAYLIST":
-          //   return{
-          //     ...state,playlist: state.playlist.map((singlePlaylist)=>{
-          //     console.log(singlePlaylist) //playlistId
-          //     console.log(payload) //videoId
-
-
-
-              
-              
-             
-          //     })
-          //   }
-          
-
-          case "ADD_TO_PLAYLIST":
-            const { playlistID, videoId } = payload;
-
-            const singlePlaylist = [...state.playlist].map((onePlaylist) => {
-                if (onePlaylist.id === playlistID) {
-                    const videoFind = !!onePlaylist.videosAdded.find((video) => video === videoId);
-                    if (videoFind) {
-                        const videoFiltered = onePlaylist.videos.filter((video) => video !== videoId)
-                        return { ...onePlaylist, videosAdded: videoFiltered }
-                    }
-                    else {
-                        const newPlaylist = { ...onePlaylist, videosAdded: [...onePlaylist.videosAdded, videoId] }
-                        return newPlaylist
-                    }
-                }
-                return onePlaylist
-            })
-
-            return { ...state, playlist: singlePlaylist }
-
-
-          
-         
-      
-          
-    
-
-      case "SEARCH":
-            return { ...state, keyword: payload.toLowerCase()}
-
-      case "CLEAR_SEARCH":
-              return { ...state, keyword: '' }
-           
-
-     
-
-    
-     
-
-      
-      
-  
-     
-  
-        default:
-          return state;
+    case "LIKE": {
+      console.log(payload);
+      return {
+        ...state,
+        likedVideos: state.likedVideos.includes(payload)
+          ? state.likedVideos.filter((id) => id !== payload)
+          : state.likedVideos.concat(payload),
+      };
     }
+
+    case "WATCH_LATER":
+      return {
+        ...state,
+        watchLater: state.watchLater.includes(payload)
+          ? state.watchLater.filter((id) => id !== payload)
+          : state.watchLater.concat(payload),
+      };
+
+    case "HISTORY":
+      return {
+        ...state,
+        historyVideos: state.historyVideos.some(
+          (videoId) => videoId === payload
+        )
+          ? state.historyVideos
+              .filter((videoId) => videoId !== payload)
+              .concat(payload)
+          : state.historyVideos.concat(payload),
+      };
+
+    case "CREATE_NEW_PLAYLIST":
+      return { ...state, playlist: [...state.playlist, payload] };
+
+    case "ADD_TO_PLAYLIST":
+      const { playlistID, videoId } = payload;
+
+      const singlePlaylist = [...state.playlist].map((onePlaylist) => {
+        if (onePlaylist.id === playlistID) {
+          const videoFind = !!onePlaylist.videosAdded.find(
+            (video) => video === videoId
+          );
+          if (videoFind) {
+            const videoFiltered = onePlaylist.videos.filter(
+              (video) => video !== videoId
+            );
+            return { ...onePlaylist, videosAdded: videoFiltered };
+          } else {
+            const newPlaylist = {
+              ...onePlaylist,
+              videosAdded: [...onePlaylist.videosAdded, videoId],
+            };
+            return newPlaylist;
+          }
+        }
+        return onePlaylist;
+      });
+
+      return { ...state, playlist: singlePlaylist };
+
+    case "DELETE_PLAYLIST": {
+      const deletePlaylist = [...state.playlist].filter(
+        (all) => all.id !== payload
+      );
+      return { ...state, playlist: deletePlaylist };
+    }
+
+    case "REMOVE_VIDEO_FROM_PLAYLIST": {
+      const { playlistId, videoID } = payload;
+
+      const removeVideoFromPlaylist = [...state.playlist].map((all) => {
+        if (all.id === playlistId) {
+          const videoFound = !!all.videos.find((video) => video === videoID);
+          if (videoFound) {
+            const filteredVideo = all.videos.filter(
+              (video) => video !== videoID
+            );
+            return { ...all, videos: filteredVideo };
+          } else {
+            return all;
+          }
+        }
+        return all;
+      });
+
+      return { ...state, playlist: removeVideoFromPlaylist };
+    }
+
+    case "SEARCH":
+      return { ...state, keyword: payload.toLowerCase() };
+
+    case "CLEAR_SEARCH":
+      return { ...state, keyword: "" };
+
+    default:
+      return state;
   }
+}
--- video-library/src/components/Playlist/Playlist.js
+++ video-library/src/components/Playlist/Playlist.js
@@ -1,76 +1,81 @@
 import React from "react";
 import { useVideo } from "../../context/VideoContext";
-import "./Playlist.css"
-
+import "./Playlist.css";
+import { useParams } from "react-router";
 
 function Playlist() {
-  const {
-    state:{playlist,videos}
-  } = useVideo();
+  const { state, dispatch } = useVideo();
+  const { playlist } = state;
+  const { id } = useParams();
 
+  const deleteVideoFromPlaylist = (playlistId, id) => {
+    dispatch({
+      type: "REMOVE_VIDEO_FROM_PLAYLIST",
+      payload: { playlistId, id },
+    });
+  };
 
+  const deletePlaylist = (id) => {
+    dispatch({ type: "DELETE_PLAYLIST", payload: id });
+  };
 
+  const findVideosInPlaylist = playlist.filter((all) => all.id === id);
+  console.log(findVideosInPlaylist);
 
   return (
-
-
     <div className="recommendedVideos">
-        <h2>Playlists</h2>
-        <div className="recommendedVideos_video" >
+      <h2>Playlists</h2>
+      <div className="recommendedVideos_video">
+        {state.playlist.map(({ name, videosAdded }) => {
+          return videosAdded.map((videoId) => {
+            const check = state.videos.find((video) => video.id === videoId);
 
-        {  playlist.map(({name,videosAdded})=>{
-          
-            
-          return videosAdded.map((videoId)=>{
-              const check = videos.find((video)=> video.id=== videoId)
-              console.log(check)
+            return (
+              <div className="playlistcard">
+                <div className="videoCard">
+                  <h3>Playlist Name:{name}</h3>
+                  <iframe
+                    src={`https://www.youtube.com/embed/${check.id}`}
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                    title="Embedded youtube"
+                  />
 
-            
-             return (
-               <div className="playlistcard">
-              <div className="videoCard">
-              <h3>Playlist Name:{name}</h3>
-              <iframe
-                src={`https://www.youtube.com/embed/${check.id}`}
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                title="Embedded youtube"
-              />
-           
-              <div className="video_info">
-                <div className="avatar">
-                  <img src={check.imageSrc} alt="" />
-                </div>
-                <div className="video-text">
-                  <h4>{check.title}</h4>
-                  <p>{check.channel}</p>
-                  <p>
-                    {check.views}.{check.timestamp}
-                  </p>
+                  <div className="video_info">
+                    <div className="avatar">
+                      <img src={check.imageSrc} alt="" />
+                    </div>
+                    <div className="video-text">
+                      <h4>{check.title}</h4>
+                      <p>{check.channel}</p>
+                      <p>
+                        {check.views}.{check.timestamp}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="buttons-container">
+                    <button
+                      onClick={() => deletePlaylist(findVideosInPlaylist[0].id)}
+                    >
+                      <i class="fa fa-trash" aria-hidden="true"></i> Playlist
+                    </button>
+                    <button
+                      onClick={() =>
+                        deleteVideoFromPlaylist(findVideosInPlaylist[0].id, id)
+                      }
+                    >
+                      <i class="fa fa-trash" aria-hidden="true"></i> Video
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-            </div>
-               
-              )
-            
-          })
-         })}
-
-       
-       
-       
-          
-        </div>
+            );
+          });
+        })}
+      </div>
     </div>
-  
-      
-  )
+  );
 }
 
 export default Playlist;
-
-
-      
-   
